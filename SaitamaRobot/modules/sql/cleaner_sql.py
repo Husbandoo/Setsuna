71152cf54b89fdcd4ebc43e1140c76d1d0f82f53
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-02 10:03:02 +0530

    update cleaner_sql.py

--- SaitamaRobot/modules/sql/cleaner_sql.py
+++ SaitamaRobot/modules/sql/cleaner_sql.py
@@ -185,9 +185,7 @@ def __load_cleaner_list():
     global CLEANER_CHATS
 
     try:
-        GLOBAL_IGNORE_COMMANDS = {
-            int(x.command) for x in SESSION.query(CleanerBlueTextGlobal).all()
-        }
+        GLOBAL_IGNORE_COMMANDS = {x.command for x in SESSION.query(CleanerBlueTextGlobal).all()}
     finally:
         SESSION.close()
 
