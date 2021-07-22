eda1e8c762e9962646980d478e3ba5401473a1ad
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-07-22 22:27:44 +0530

    fixes and fixes

--- Setsuna/modules/helper_funcs/telethn/chatstatus.py
+++ Setsuna/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from Setsuna.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from Setsuna import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
